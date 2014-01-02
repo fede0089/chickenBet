d5a67945d0d4cc62217a302b74043a154bd2cfa1
Federico Mete <federico.mete@mercadolibre.com>
2014-01-02 16:59:51 -0300

    Grails upgraded to 2.3.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,7 +19,7 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    legacyResolve true // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
@@ -45,7 +45,10 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+
+
+        //runtime ":hibernate:$grailsVersion"
+        runtime ':hibernate:3.6.10.6'
         //runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
         // Uncomment these (or add new ones) to enable additional resources capabilities
@@ -53,8 +56,8 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
-
+       // build ":tomcat:$grailsVersion"
+        build ':tomcat:7.0.47'
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
