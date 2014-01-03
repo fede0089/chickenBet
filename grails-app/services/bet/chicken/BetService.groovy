535786e4401cc95c09c6560789af46e4c6a0f3b2
Federico Mete <fedee.1989@gmail.com>
2014-01-02 22:51:29 -0300

    CheckWinners changes

--- grails-app/services/bet/chicken/BetService.groovy
+++ grails-app/services/bet/chicken/BetService.groovy
@@ -1,38 +1,60 @@
 package bet.chicken
+
+import java.util.logging.Logger;
+
 class BetException extends RuntimeException{
 	String message
-	Bet bet	
+	Bet bet
 }
 
 class BetService {
 
-    def createBet(BetCommandObject bco,String alias) {
+	def createBet(BetCommandObject bco,String alias) {
 
 		def player = Player.findByAlias(alias)
 		def bet = new Bet(numbers:bco.numbers)
 		if (player)
 			player.addToCurrentBets(bet).save()
-		else 		
+		else
 			new Player(alias:alias).addToCurrentBets(bet).save()
 
-    }
-	
+	}
+
 	def list(){
 		Bet.list()
 	}
-	
+
 	def checkWinners(BetCommandObject bco){
 		def results = bco.numbers
 		def allBets = list()
+
+		//Iterates over all bets
 		def allMatches = allBets.collect{bet->
-			[winnings:(bet.numbers.count {number-> results.contains(number)}),player:bet.player]
+
+			//New array for missing Numbers
+			def missing =[]
+			missing.addAll(bet.numbers)
+			
+			//New array for matched Numbers
+			def matched = []
+
+			//Return winners map with [winnings : aciertos, player: jugador, missing: numeros que faltaron,matched: numeros acertados]
+			def quantity = bet.numbers.count {number->
+				if (results.contains(number)){
+					matched.add(number)					
+					missing.remove(number as Object)
+				}
+
+			}
+			
+			[winnings:quantity,player:bet.player,missing:missing,matched:matched]
 		}
-		
+
 		allMatches.groupBy {winner->
 			winner.winnings
 		}
-		
-		
-		
+
+
+
 	}
 }
--- grails-app/views/bet/checkWinners.gsp
+++ grails-app/views/bet/checkWinners.gsp
@@ -2,16 +2,33 @@
 	<meta name="layout" content="main" />
 </head>
 <body>
-	<h2>7 aciertos:</h2>
-	<g:each in="${winners[7]}" var="winner">
-		<h4>${winner.player.alias}</h4>
-	</g:each>
-	<h2>6 aciertos:</h2>
-	<g:each in="${winners[6]}" var="winner">
-		<h4>${winner.player.alias}</h4>
-	</g:each>
-	<h2>5 aciertos:</h2>
-	<g:each in="${winners[5]}" var="winner">
-		<h4>${winner.player.alias}</h4>
-	</g:each>
+		<h1 class="text-center">Resultados</h1>
+		<ul>	
+		<g:each in="${7..5}" var="quantity">
+		
+				<div class="panel panel-default">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">${quantity} aciertos</h3>
+				  </div>
+				  <div class="panel-body">
+				    	<g:if test="${winners[quantity]}">
+							<g:each in="${winners[quantity]}" var="winner">
+								<span class="label label-success winnerPopover" data-title="Jugada" data-trigger="hover" data-html=true
+								data-content="<b>${winner.matched.join(',')}${quantity<7?',':''}<span style='color:red;'>${winner.missing.join(',')}</span></b>" >${winner.player.alias}</span>
+							</g:each>
+						</g:if>
+						<g:else>
+							<span class="label label-danger">VACANTE</span>
+						</g:else>
+				  </div>
+				</div>			
+		</g:each>
+		</ul>
+		<g:link class="btn btn-danger btn-lg btn pull-right"  >Volver</g:link>
+		
+		<g:javascript>
+		$(document).ready(function(){
+			$('.winnerPopover').popover();
+		});
+		</g:javascript>
 </body>
