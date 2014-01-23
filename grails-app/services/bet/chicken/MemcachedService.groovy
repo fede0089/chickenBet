523fc6cdba29c3809f5cc262690e9747e16335b8
Federico Mete <federico.mete@mercadolibre.com>
2014-01-23 17:41:48 -0300

    Memcached service added

---
+++ grails-app/services/bet/chicken/MemcachedService.groovy
@@ -0,0 +1,49 @@
+package bet.chicken
+import net.spy.memcached.MemcachedClient
+import org.springframework.beans.factory.InitializingBean
+import net.spy.memcached.AddrUtil
+
+
+import grails.transaction.Transactional
+
+@Transactional
+   class MemcachedService implements InitializingBean {
+
+    static final Object NULL = "NULL"
+    def MemcachedClient memcachedClient
+
+    def void afterPropertiesSet() {
+        memcachedClient = new MemcachedClient(AddrUtil.getAddresses("localhost:11211"))
+    }
+
+    def get(String key) {
+        return memcachedClient.get(key)
+    }
+
+    def set(String key, Object value) {
+        memcachedClient.set(key, 600, value)
+    }
+
+    def delete(String key) {
+        memcachedClient.delete(key)
+    }
+
+    def clear() {
+        memcachedClient.flush()
+    }
+
+    def update(key, function) {
+        def value = function()
+        if (value == null) value = NULL
+        set(key, value)
+        return value
+    }
+
+    def get(key, function) {
+        def value = get(key)
+        if (value == null) {
+            value = update(key, function)
+        }
+        return (value == NULL) ? null : value;
+    }
+}
