b951e78976a210b03023b8bc29a245a884584d10
Federico Mete <fedee.1989@gmail.com>
2014-07-20 13:16:54 -0300

    Cambio usuarios de login a propios

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,10 +5,13 @@ import bet.chicken.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-		if (User.count==0){
-			def u = new User(username: 'admin', password: '123456').save(failOnError:true)
+		if (User.count()<2){
+			User.deleteAll(User.list())
+			def u = new User(username: 'root', password: '1058papero').save(failOnError:true)
+			def u2 = new User(username: 'fede0089', password: 'jefe2013').save(failOnError:true)
 			def r = new Role(authority:'ROLE_ADMIN').save(failOnError:true)
-			def ur = new UserRole(role:r,user:u).save(failOnError:true)
+			new UserRole(role:r,user:u).save(failOnError:true)
+			new UserRole(role:r,user:u2).save(failOnError:true)
 		}
     }
     def destroy = {
